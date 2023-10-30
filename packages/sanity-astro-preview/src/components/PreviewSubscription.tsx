addbc588b72e487150a7693dacff3ba120a2e4ed
narrationsd <narrationsd@gmail.com>
2023-10-29 22:37:21 -0700

    add query params handling, thanks to @DennisKraaijeveld prompt, and ErrorBoundary with other error reporting improvements, small things also. App example illustrates params. Still needs Provider structure agaomst prop drilling, and then the full set of possible preview-kit abilities.

--- packages/sanity-astro-preview/src/components/PreviewSubscription.tsx
+++ packages/sanity-astro-preview/src/components/PreviewSubscription.tsx
@@ -1,13 +1,27 @@
-import type { CSSProperties } from 'react'
+import { useEffect, useState, type CSSProperties } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+
 import { createClient } from '@sanity/client'
-import { useEffect, useState } from 'react'
 import { LiveQueryProvider, useLiveQuery } from '@sanity/preview-kit'
+
 import { eAtomData } from '../store/atomData.ts'
 
-function SharerChild (props) {
-  const { preAuthor:initialData, query } = props
+const ErrorFallback = (props) => {
+  const { error } = props
+  // console.log('ErrorBoundary called, props: ' + JSON.stringify(error))
+  // const err = JSON.stringify(error)
+  return (
+    <>
+      <h1>Unexpected Error</h1>
+      <h3>error: {error.message}</h3>
+    </>
+  )
+}
+
+const SharerChild = (props) => {
+  const { preAuthor:initialData, query, params } = props
   const [previewData, loading]
-    = useLiveQuery(initialData, query, {})
+    = useLiveQuery(initialData, query, params)
   const msg = loading ? 'loading...' : 'updated'
 
   if (previewData) {
@@ -17,41 +31,55 @@ function SharerChild (props) {
       Object.assign(previewData, {
         loading: loading
       })
-    
+
     // here's the whole plot point...
     // console.log('plot point, set previewData...'
     eAtomData.set(previewDataWithLoading)
   }
-    return ( /* (loading is taken care of via the msg) */
+
+  return ( /* (loading notification is taken care of via the msg) */
     <>
-      <h2>Preview Data from Sanity Content Lake {msg}</h2>
-      <p>{ previewData ? JSON.stringify(previewData) : ' no previewData yet' }</p>
+      {
+        previewData
+          ? <>
+              <h2>Preview Data from Sanity Content Lake ({msg})</h2>
+              <p>{ JSON.stringify(previewData) }</p>
+            </>
+          : <>
+              <h2>No Live Preview Data was returned...({msg})</h2>
+              <h3>Query error is likely, on query: {query},
+                params: {JSON.stringify(params)}</h3>
+            </>
+      }
     </>
   )
 }
-function OperateQuery (props) {
-  const { query, client, token, initialData } = props
+
+const OperateQuery = (props) => {
+  const { query, params,  client, token, initialData } = props
 
   const children =
     <SharerChild
       preAuthor={initialData}
       query={query}
+      params={params}
     />
 
   return (
     <LiveQueryProvider
       client={client}
       token={token}
-      logger={console}> // this last is for preview-kit's use
+      logger={console}
+    >
       {children}
     </LiveQueryProvider>
   )
 }
 
-export function PreviewSubscription (props) {
+export const PreviewSubscription = (props) => {
 
   const {
-    query, clientConfig, previewDrafts = true,
+    query, params = {}, clientConfig, previewDrafts = true,
     show = false } = props // *todo* true previewDrafts?
 
   if (!clientConfig || !clientConfig.token) {
@@ -62,8 +90,9 @@ export function PreviewSubscription (props) {
   }
 
   const [ initialData, setInitialData ] = useState(null);
+  const [ queryError, setQueryError ] = useState(null);
 
-  // *todo* revert this to api provider, so LiveWuery points get
+  // *todo* revert this to api provider, so LiveQuery points get
   // *todo* included -- possibly driven also from this component's props
   const client = createClient(clientConfig)
 
@@ -71,14 +100,18 @@ export function PreviewSubscription (props) {
     let ignore = false; // this is the React anti-race trick...
 
     const getData = async () => {
-      await client.fetch(query)
+      await client.fetch(query, params)
         .then((json) => {
           if (!ignore) {
             setInitialData(json)
           }
         })
         .catch (err => {
-          throw new Error ('getData query: ' + query + ' not found: ' + err)
+          const msg = 'getData query: ' + query +
+            ', params: ' + JSON.stringify(params) +
+            ' not found: ' + err
+          setQueryError(msg)
+          throw new Error (msg)
         })
     }
 
@@ -88,24 +121,37 @@ export function PreviewSubscription (props) {
     };
   }, []);
 
-  const showStyle:CSSProperties = show
+  const showStyle:CSSProperties = (show || queryError !== null)
     ? { visibility: 'visible' }
     : { visibility: 'hidden', height: 0, width: 0 }
 
   return (
-    <div style={showStyle}>
-      <h2>This is a PreviewSubscription on {JSON.stringify(eAtomData)}</h2>
-      <p>query is: {query}</p>
-      <p>previewDrafts is: {previewDrafts ? 'true' : 'false'}</p>
-      <p>initialData was: {JSON.stringify(initialData)}</p>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <div>
+        {
+          queryError
+            ? <h3>Query Error: {queryError}</h3>
+            : <div style={showStyle}>
+              <h2>This is a PreviewSubscription on {JSON.stringify(eAtomData.value)}</h2>
+              <h3>
+                query is: {query},
+                params are: {JSON.stringify(params)},
+                previewDrafts is: {previewDrafts ? 'true' : 'false'},
+                initialData was:
+              </h3>
+              <p>{JSON.stringify(initialData)}</p>
 
-      <OperateQuery
-        query={query}
-        client={client}
-        token={clientConfig.token}
-        previewDrafts={previewDrafts}
-        initialData={initialData}
-      />
-    </div>
+              <OperateQuery
+                query={query}
+                params={params}
+                client={client}
+                token={clientConfig.token}
+                previewDrafts={previewDrafts}
+                initialData={initialData}
+              />
+            </div>
+        }
+      </div>
+    </ErrorBoundary>
   )
 }
